cc18f8b1f8a78861b16e4845be5d74223910f815
Reed Brooks <reedb36@gmail.com>
2020-09-23 14:01:34 -0700

    Begin working on basic information display for the user

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("test");
+const prompt = require('prompt');
+
+//initialize variables
+
+const board = [['-','-','-'],
+              ['-','-','-'],
+              ['-','-','-']];
+
+const options = [[1,2,3],
+                 [4,5,6],
+                 [7,8,9]];
+
+let xTurn = true;
+let winner = undefined;
+
+while(winner === undefined) {
+  console.log(board[0]);
+  console.log(board[1]);
+  console.log(board[2]);
+  let activePlayer = xTurn === true ? 'X' : 'O';
+  console.log(`It is player ${activePlayer}'s turn.`)
+  console.log(options[0]);
+  console.log(options[1]);
+  console.log(options[2]);
+
+  winner = 1;
+}
